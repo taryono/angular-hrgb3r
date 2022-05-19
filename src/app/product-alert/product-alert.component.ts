ede4c32f605cbad5b4f7084290c078a4c2f82ccb
taryono <denmas.yono@gmail.com>
2022-05-19 10:37:48 +0700

    update

---
+++ src/app/product-alert/product-alert.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input,Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alert',
+  templateUrl: './product-alert.component.html',
+  styleUrls: ['./product-alert.component.css']
+})
+export class ProductAlertComponent implements OnInit {
+  /** Baca inputan product */
+  @Input() product:Product | undefined;
+  @Output() notify = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
